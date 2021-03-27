f44b169c26ba1cd036587a715994c1f099e40225
Evgeni Pandurski <epandurski@gmail.com>
2021-03-28 00:34:32 +0200

    Minor refactoring

--- src/TestOauth2.svelte
+++ src/TestOauth2.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import type {DebtorReservationRequest} from './schema'
   import {oauth} from './oauth2'
-  import {json_stringify} from './json-bigint'
-  import {json_parse} from './json-bigint'
+  import {stringify} from './json-bigint'
+  import {parse} from './json-bigint'
 
   const big = BigInt('123456789012345678901234')
   let x: DebtorReservationRequest = {}
@@ -13,8 +13,8 @@
   }
 </script>
 
-<h1>{json_stringify(big)}</h1>
-<h1>{json_parse('123456789012345678901234')}</h1>
-<h1>{json_parse('12345678901.2345678901234')}</h1>
-<h1>{json_parse('1e2')}</h1>
+<h1>{stringify(big)}</h1>
+<h1>{parse('123456789012345678901234')}</h1>
+<h1>{parse('12345678901.2345678901234')}</h1>
+<h1>{parse('1e2')}</h1>
 <button on:click={authorize}>Test!</button>
--- src/json-bigint/index.ts
+++ src/json-bigint/index.ts
@@ -1,4 +1,3 @@
-import { json_parse as json_parse_factory } from './parse.js'
-
-export const json_parse = json_parse_factory()
-export { stringify as json_stringify } from './stringify.js'
+import { json_parse } from './parse.js'
+export { stringify } from './stringify.js'
+export const parse = json_parse()
