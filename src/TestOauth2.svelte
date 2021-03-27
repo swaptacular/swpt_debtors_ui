bfa483732c8e3fc95f992675075f47a4954ed850
Evgeni Pandurski <epandurski@gmail.com>
2021-03-27 21:43:35 +0200

    Add types in schema.ts

--- src/TestOauth2.svelte
+++ src/TestOauth2.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  import type {DebtorReservationRequest} from './schema'
   import {oauth} from './oauth2'
   
+  let x: DebtorReservationRequest = {}
+  console.log(x.type)
+
   function authorize() {
     oauth.fetchAuthorizationCode()
   }
