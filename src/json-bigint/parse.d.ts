b15975f22deec3026ba5cbc91444767a84e22114
Evgeni Pandurski <epandurski@gmail.com>
2021-05-16 00:37:22 +0300

    minor beautification

--- src/json-bigint/parse.d.ts
+++ src/json-bigint/parse.d.ts
@@ -1,10 +1,10 @@
-type ParseOptions = {
-  strict?: boolean;
-  storeAsString?: boolean;
-  protoAction?: 'error' | 'ignore' | 'preserve';
-  constructorAction?: 'error' | 'ignore' | 'preserve';
+export type ParseOptions = {
+  strict?: boolean,
+  storeAsString?: boolean,
+  protoAction?: "error" | "ignore" | "preserve",
+  constructorAction?: "error" | "ignore" | "preserve",
 }
-type ReviverFunction = ((string, any) => any)
-type ParseFunction = ((source: string, reviver?: ReviverFunction) => any)
+export type ReviverFunction = (string, any) => any
+export type ParseFunction = (source: string, reviver?: ReviverFunction) => any
 
 export declare function json_parse(options?: ParseOptions): ParseFunction
--- src/json-bigint/stringify.d.ts
+++ src/json-bigint/stringify.d.ts
@@ -1,5 +1,5 @@
-type ReplacerFunction = ((key: string, value: any) => any)
-type ReplacerArray = any[]
-type Replacer = ReplacerFunction | ReplacerArray
+export type ReplacerFunction = (key: string, value: any) => any
+export type ReplacerArray = any[]
+export type Replacer = ReplacerFunction | ReplacerArray
 
 export declare function stringify(value: any, replacer?: Replacer, space?: string | number): string
