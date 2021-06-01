4db67446dbe64d864dfc3d3aec26fc5170e89550
Evgeni Pandurski <epandurski@gmail.com>
2021-06-01 22:36:05 +0300

    Delete old file

--- tests/oauth2-auth-code-pkce.test.ts
+++ tests/oauth2-auth-code-pkce.test.ts
@@ -1,4 +1,4 @@
-import { OAuth2AuthCodePKCE } from '../src/oauth2-auth-code-pkce/index.js'
+import { OAuth2AuthCodePKCE } from '../src/oauth2/oauth2-auth-code-pkce.js'
 
 test("Extract parameter from URL", () => {
   const value = OAuth2AuthCodePKCE.extractParamFromUrl('https://example.com?value1=key1&key1=value1', 'key1')
