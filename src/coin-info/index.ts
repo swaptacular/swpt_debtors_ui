37bec770227aa5fe517a19b5c90474ad433a7130
Evgeni Pandurski <epandurski@gmail.com>
2021-07-15 01:57:41 +0300

    More work on coin-info/

--- src/coin-info/index.ts
+++ src/coin-info/index.ts
@@ -1,7 +1,5 @@
 import validate from './validate-schema.js'
 
-const MAX_INT64 = 2n ** 63n - 1n
-const MIN_INT64 = -(2n ** 63n)
 const UTF8_ENCODER = new TextEncoder()
 
 export type ResourceReference = {
@@ -10,13 +8,12 @@ export type ResourceReference = {
 }
 
 export type DebtorIdentity = {
+  type: 'DebtorIdentity',
+
   /** The information contained in this field must be enough to
    * uniquely and reliably identify the debtor. Note that a network
    * request *should not be needed* to identify the debtor. */
   uri: string,
-
-  /** The type of this object. */
-  type: 'DebtorIdentity',
 }
 
 export type CoinPeg = {
@@ -47,35 +44,25 @@ export class InvalidCoinInfo extends Error {
 
 export const MIME_TYPE_COIN_INFO = 'application/vnd.swaptacular.coin-info+json'
 
-function validateCoinInfo(coinInfo: CoinInfo): void {
-  const INVALID = ['Invalid value.']
-  let errors: { [field in keyof CoinInfo]+?: string[] } = {}
-
-  const type_ = coinInfo.uri
-  if (type_ !== 'CoinInfo') {
-    errors.type = INVALID
-  }
-  const uri = coinInfo.uri
-  if (typeof uri !== 'string' || uri.length > 200) {
-    errors.uri = INVALID
-  }
-  const revision = coinInfo.revision
-  if (typeof revision !== 'bigint' || revision < MIN_INT64 || revision > MAX_INT64) {
-    errors.revision = INVALID
-  }
-}
-
 export function generateCoinInfoBlob(coinInfo: CoinInfo): Blob {
-  validateCoinInfo(coinInfo)
-  const content = '' //UTF8_ENCODER.encode(stringify(coinInfo))
+  if (coinInfo.validUntil && Number.isNaN(coinInfo.validUntil.getTime())) {
+    throw new InvalidCoinInfo('invalid validUntil')
+  }
+  const validUntil = coinInfo.validUntil?.toISOString()
+  const data = { ...coinInfo, validUntil }
+  if (!validate(data)) {
+    const e = validate.errors[0]
+    throw new InvalidCoinInfo(`${e.instancePath} ${e.message}`)
+  }
+  const content = UTF8_ENCODER.encode(JSON.stringify(coinInfo))
   return new Blob([content], { type: MIME_TYPE_COIN_INFO })
 }
 
 export async function parseCoinInfoBlob(blob: Blob): Promise<CoinInfo> {
-  let coinInfo
   if (blob.type && blob.type !== MIME_TYPE_COIN_INFO) {
     throw new InvalidCoinInfo('wrong content type')
   }
+  let coinInfo
   try {
     coinInfo = JSON.parse(await blob.text()) as CoinInfo
   } catch (e: unknown) {
@@ -83,7 +70,7 @@ export async function parseCoinInfoBlob(blob: Blob): Promise<CoinInfo> {
   }
   if (!validate(coinInfo)) {
     const e = validate.errors[0]
-    throw new InvalidCoinInfo(`${e.schemaPath} ${e.message}`)
+    throw new InvalidCoinInfo(`${e.instancePath} ${e.message}`)
   }
   const isoValidUntil = coinInfo.validUntil
   const validUntil = isoValidUntil ? new Date(isoValidUntil) : undefined
