87ff4623d64f4edaf6ec99c2fdd823d0ba99611f
Evgeni Pandurski <epandurski@gmail.com>
2021-07-28 20:07:49 +0300

    Add doc comment

--- src/stores.ts
+++ src/stores.ts
@@ -1,5 +1,10 @@
 import type { Observable } from 'dexie'
 
+/* Returns a promise for an object that satisfies Svelte's store
+ * contract. Svelte stores are required to call the `onNext` method
+ * synchronously, but observables are not required to do so. This
+ * function awaits for the first value on the observable to appear, so
+ * that the created store can return it on subscription. */
 export async function createStore<T>(observable: Observable<T>) {
   let onNext: any
   let onError: any
