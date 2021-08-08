d28c821bce86f97516e064865ca1aab342abf1d2
Evgeni Pandurski <epandurski@gmail.com>
2021-08-08 19:52:39 +0300

    Work on UpdateConfigAction.svelte

--- src/components/UpdateConfigAction.svelte
+++ src/components/UpdateConfigAction.svelte
@@ -4,10 +4,38 @@
 
   export let app: AppState
   export let action: UpdateConfigActionWithId
+  let isDirty = false
+  let interestRate = action.interestRate ?? 0
+  let summary = action.debtorInfo?.summary ?? ''
+  let debtorName = action.debtorInfo?.debtorName ?? ''
+  let debtorHomepageUri = action.debtorInfo?.debtorHomepage?.uri ?? ''
+  let amountDivisor = action.debtorInfo?.amountDivisor ?? 100
+  let decimalPlaces = action.debtorInfo?.decimalPlaces ?? 2
+  let unit = action.debtorInfo?.unit ?? ''
+  let peg = action.debtorInfo?.peg
 
   // TODO: call this on change, and when leaving the page.
   function save() {
-    actionManager.save(updatedAction)
+    actionManager.save({
+      ...action,
+      interestRate,
+      debtorInfo: {
+        summary: summary || undefined,
+        debtorName,
+        debtorHomepage: debtorHomepageUri ? { uri: debtorHomepageUri } : undefined,
+        amountDivisor: amountDivisor | Number.EPSILON,
+        decimalPlaces: Math.round(decimalPlaces),
+        unit,
+        peg,
+      }
+    })
+    isDirty = false
+  }
+  function markDirty() {
+    if (!isDirty) {
+      setTimeout(save, 2000)
+      isDirty = true
+    }
   }
   function execute() {
     save()
@@ -17,15 +45,19 @@
     actionManager.remove()
   }
 
-  $: updatedAction = action
   $: actionManager = app.createActionManager(action)
 </script>
 
 <h1>Update Config Action</h1>
-<dl>
-  <dt>actionId:</dt> <dd>{action.actionId}</dd>
-  <dt>createdAt:</dt> <dd>{action.createdAt.toISOString()}</dd>
-</dl>
+<form on:input={markDirty}>
+  <p><label>interestRate:<input required type=number bind:value={interestRate}></label></p>
+  <p><label>debtorName:<input required minlength="1" maxlength="40" bind:value={debtorName}></label></p>
+  <p><label>Summary:<textarea bind:value={summary} maxlength="1000"></textarea></label></p>
+  <p><label>debtorHomepage:<input maxlength="10000" bind:value={debtorHomepageUri}></label></p>
+  <p><label>amountDivisor:<input required type=number min="0" bind:value={amountDivisor}></label></p>
+  <p><label>decimalPlaces:<input required type=number min="-20" max="20" step="1" bind:value={decimalPlaces}></label></p>
+  <p><label>unit:<input required minlength="1" maxlength="40" bind:value={unit}></label></p>
+</form>
 
 <button on:click={dismiss}>Dismiss</button>
 <button on:click={execute}>Save</button>
