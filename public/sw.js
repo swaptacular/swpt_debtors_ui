96313524c1a919aa8684a3b849f9d494116037a3
Evgeni Pandurski <epandurski@gmail.com>
2021-09-18 21:21:05 +0300

    Add save button to the transfer page

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const appName = 'swpt-debtors-ui'
-const cacheName = `${appName}-v0.2.1`
+const cacheName = `${appName}-v0.2.2`
 const appFiles = [
   './',
   'index.html',
--- src/components/TransferPage.svelte
+++ src/components/TransferPage.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import { onDestroy } from 'svelte'
+  import { generatePr0Blob } from '../payment-requests'
   import type { TransferRecord } from '../operations'
   import { getTooltip } from '../utils'
   import type { AppState, TransferModel } from '../app-state'
-  import Fab, { Label } from '@smui/fab';
+  import Fab, { Icon, Label } from '@smui/fab';
   import PaymentInfo from './PaymentInfo.svelte'
   import Page from './Page.svelte'
 
@@ -11,6 +13,8 @@
   export let snackbarBottom: string
 
   const maxUnitAmount =  Number(app.amountToString(2n ** 63n - 1000000n))
+  let downloadLinkElement: HTMLAnchorElement
+  let currentDataUrl: string
 
   function getTitle(t: TransferRecord): string {
     switch (true) {
@@ -22,17 +26,43 @@
       return 'Initiated payment'
     }
   }
+  function revokeCurrentDataUrl() {
+    if (currentDataUrl) {
+      URL.revokeObjectURL(currentDataUrl)
+    }
+  }
+  function generateDataUrl(t: TransferRecord): string {
+    const blob = generatePr0Blob({
+      ...t.paymentInfo,
+      accountUri: t.recipient.uri,
+      amount: t.noteFormat === 'PAYMENT0' ? t.amount : 0n,
+
+      // NOTE: The `deadline` field is not set, because
+      // deadlines are not supported in the Web API.
+    })
+    revokeCurrentDataUrl()
+    return currentDataUrl = URL.createObjectURL(blob)
+  }
+
+  onDestroy(revokeCurrentDataUrl)
 
   $: transfer = model.transfer
   $: snackbarBottom = $transfer.result ? '84px' : '0px'
   $: payeeName = $transfer.paymentInfo.payeeName
+  $: payeeReference = $transfer.paymentInfo.payeeReference
   $: unitAmount = app.amountToString($transfer.amount)
   $: description = $transfer.paymentInfo.description
   $: title = getTitle($transfer)
   $: tooltip = getTooltip($transfer)
+  $: dataUrl = generateDataUrl($transfer)
+  $: downloadName = payeeReference ? `${payeeName} - ${payeeReference}` : `${payeeName}`
+  $: fileName = downloadName.slice(0, 60) + '.pr0'
 </script>
 
 <style>
+  .download-link {
+    display: none;
+  }
   .fab-container {
     margin: 16px 16px;
   }
@@ -65,5 +95,12 @@
         </Fab>
       </div>
     {/if}
+    <div class="fab-container">
+      <a class="download-link" href={dataUrl} download={fileName} bind:this={downloadLinkElement}>download</a>
+      <Fab color="primary" on:click={() => downloadLinkElement.click()} extended>
+        <Icon class="material-icons">download</Icon>
+        <Label>Save</Label>
+      </Fab>
+    </div>
   </svelte:fragment>
 </Page>
