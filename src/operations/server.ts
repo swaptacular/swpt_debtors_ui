d3fd972c50537977e4ffc68df564673accad0b61
Evgeni Pandurski <epandurski@gmail.com>
2022-05-30 16:20:43 +0300

    Add todo

--- src/operations/server.ts
+++ src/operations/server.ts
@@ -4,6 +4,11 @@ export * from '../web-api-schemas'
 
 export const server = new ServerSession({
   onLoginAttempt: async (login) => {
+    // TODO: Consider not using `confirm()` or `alert()` anywhere.
+    // These functions pause the event loop, which may result in
+    // indexDB locks being held indefinitely, or broadcast channel
+    // messages not being processed. It is not clear whether,
+    // though, if this is a practical problem or not.
     if (confirm('This operation requires authentication. You will be redirected to the login page.')) {
       return await login()
     }
